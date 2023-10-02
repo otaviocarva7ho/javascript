758ac7559ea1cb0c3812b60c42d338e752baa01f
otaviocarva7ho <carvalhom7otavioh@gmail.com>
2023-10-02 18:57:47 -0300

    Repetições 01

---
+++ 13/ambiente.js
@@ -0,0 +1,24 @@
+let c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6);
+
+
+/*
+let c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
