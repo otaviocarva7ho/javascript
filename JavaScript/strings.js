597444923c2fb60a876106ee901b6a327a5e8c82
otaviocarva7ho <carvalhom7otavioh@gmail.com>
2023-10-27 19:49:43 -0300

    Var e Strings

---
+++ JavaScript/strings.js
@@ -0,0 +1,26 @@
+var nome = 'Programador a bordo!'
+console.log(nome)
+
+console.log(nome.length)
+
+console.log('Programador'.length)
+
+console.log(nome.toUpperCase())
+
+console.log(nome.toLowerCase())
+
+console.log(nome.indexOf('dor'))
+
+console.log(nome.indexOf('teste'))
+
+console.log(nome.replace('Programador', 'Desenvolvedor'))
+
+console.log(nome.slice(3, 8))
+
+var canalYoutube = '     Programador a Bordo'
+console.log(canalYoutube)
+console.log(canalYoutube.trim())
+
+var autor = 'Carvalho'
+console.log('O canal do YouTube ' + canalYoutube.trim() + 'foi criado por ' + autor)
+console.log(`O canal do YouTube ${canalYoutube.trim()} foi criado por ${autor}`)
---
+++ JavaScript/var.js
@@ -0,0 +1,14 @@
+var nome = 'Carvalho'
+console.log(nome)
+
+var idade = 26
+console.log(idade)
+
+var total = idade + 10
+console.log(total)
+
+total = 50
+console.log(total)
+
+var total = 100
+console.log(total)
